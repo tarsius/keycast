adb55497c0f16e90069d6e5e86b4f9f65f13624b
Jonas Bernoulli <jonas@bernoul.li>
2020-01-03 13:39:34 +0100

    Bump copyright years

--- keycast.el
+++ keycast.el
@@ -1,6 +1,6 @@
 ;;; keycast.el --- Show current command and its key in the mode line  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2018-2019  Jonas Bernoulli
+;; Copyright (C) 2018-2020  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/keycast
