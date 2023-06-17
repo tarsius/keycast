0b16d6aa3ae965bcd6b4d1fd0173cfde9dc8e275
Jonas Bernoulli <jonas@bernoul.li>
2023-06-17 13:28:37 +0200

    Release version 1.3.2

--- keycast.el
+++ keycast.el
@@ -7,7 +7,7 @@
 ;; Keywords: multimedia
 
 ;; Package-Requires: ((emacs "25.3") (compat "29.1.4.1"))
-;; Package-Version: 1.3.1
+;; Package-Version: 1.3.2
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
