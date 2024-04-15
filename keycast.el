04fa2c65f0ae901ed3015f691ea70f7658ea24b8
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:39:27 +0200

    Update contact information

--- keycast.el
+++ keycast.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.keycast@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/tarsius/keycast
 ;; Keywords: multimedia
 
