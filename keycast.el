c44618d2867fc2410e5061fef2a805e974198cf2
Jonas Bernoulli <jonas@bernoul.li>
2024-08-05 15:22:39 +0200

    Release version 1.4.1

--- keycast.el
+++ keycast.el
@@ -6,8 +6,8 @@
 ;; Homepage: https://github.com/tarsius/keycast
 ;; Keywords: multimedia
 
+;; Package-Version: 1.4.1
 ;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
-;; Package-Version: 1.4.0
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
